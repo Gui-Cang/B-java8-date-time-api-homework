0c963110c36a5267ac601a65036a9dd7924d531f
Gui-Cang <821192514@qq.com>
2020-11-17 00:45:05 +0800

    [dingke] feat: finish practice1 by toEpochDay and ChronoUnit.

--- src/main/java/com/thoughtworks/capability/gtb/Practice1.java
+++ src/main/java/com/thoughtworks/capability/gtb/Practice1.java
@@ -1,6 +1,7 @@
 package com.thoughtworks.capability.gtb;
 
 import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 
 /**
  * 计算任意日期与下一个劳动节相差多少天
@@ -10,7 +11,25 @@ import java.time.LocalDate;
  */
 public class Practice1 {
 
-  public static long getDaysBetweenNextLaborDay(LocalDate date) {
-    return 0;
-  }
+    public static long getDaysBetweenNextLaborDay(LocalDate date) {
+//    Long result = 0L;
+//    int year = date.getYear();
+//    LocalDate mayDayThisYear = LocalDate.of(year,5,1);
+//    if (date.isBefore(mayDayThisYear)) {
+//      result = ChronoUnit.DAYS.between( date, mayDayThisYear);
+//    }else {
+//      result = ChronoUnit.DAYS.between( date, LocalDate.of(year+1, 5, 1));
+//    }
+//    return (Long) result;
+        Long result;
+        int year = date.getYear();
+        LocalDate mayDayThisYear = LocalDate.of(year, 5, 1);
+        LocalDate mayDayNextYear = LocalDate.of(year + 1, 5, 1);
+        if (date.isBefore(mayDayThisYear)) {
+            result = mayDayThisYear.toEpochDay() - date.toEpochDay();
+        } else {
+            result = mayDayNextYear.toEpochDay() - date.toEpochDay();
+        }
+        return (Long) result;
+    }
 }
